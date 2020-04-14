6c6fcc80158a3dcb10abadf4e56e9d193a9024e8
Chris McMeeking <chris.mcmeeking@deque.com>
2020-04-14 13:13:46 -0400

    fix: Get gpg secrets from env variables.

--- .circleci/circle_publish_release.sh
+++ .circleci/circle_publish_release.sh
@@ -2,5 +2,11 @@
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )
 source ./env.sh
 
+echo "$DQ_AXE_GPG_KEY" | base64 -d > ./secret.gpg
+
 ./gradlew artifactoryPublish -PversionName=$1
-./gradlew uploadArchives -PversionName=$1
+./gradlew uploadArchives \
+  -PversionName=$1 \
+  -Psigning.secretKeyRingFile=./secret.gpg \
+  -Psigning.keyId=${DQ_AXE_GPG_SINGING_KEY_ID} \
+  -Psigning.password=${DQ_AXE_GPG_SIGNING_PASSWORD}
