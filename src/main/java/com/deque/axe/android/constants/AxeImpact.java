6f0cc2168c09c89b62b883c41fd7da3915817083
Devanshu <59444071+devchan4188@users.noreply.github.com>
2020-11-11 13:46:06 -0500

    breaking: Converts AxeImpact to enum (#120)

--- src/main/java/com/deque/axe/android/constants/AxeImpact.java
+++ src/main/java/com/deque/axe/android/constants/AxeImpact.java
@@ -4,9 +4,13 @@ package com.deque.axe.android.constants;
  * Enums in Android are sketchy, so we have this simple Static class for accessing Impact constants.
  */
 public enum AxeImpact {
-  MINOR(0), MODERATE(1), SERIOUS(2), CRITICAL(3), BLOCKER(4);
+  MINOR(0),
+  MODERATE(1),
+  SERIOUS(2),
+  CRITICAL(3),
+  BLOCKER(4);
 
-  private int impact;
+  private final int impact;
 
   AxeImpact(int impact) {
     this.impact = impact;
